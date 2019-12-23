3591b73f9c0cebf02ebb2046c84164fadbd4cc6d
tlewandowski18 <tlewandowski18@gmail.com>
2019-12-22 22:33:53 -0500

    Export and import data; create Specials, Numbers, and Operators components; export components; import components into App.js and render

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
   return (
-    <>
+    <button className="operator">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.text}
+    </button>
   );
 };
+
+export default OperatorButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { specials } from "../../../data";
+import SpecialButton from "./SpecialButton";
+
 
 //import any components needed
 
@@ -6,12 +9,18 @@ import React from "react";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
-    <div>
+    <div className="specials-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specials.map(special => {
+         return <SpecialButton text={special} />
+       })}
     </div>
   );
 };
+
+export default Specials;
